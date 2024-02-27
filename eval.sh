b0eb2f023756f954b6f9d421a5349c4b51ea0c49
WANGXinyiLinda <xinyi_wang@ucsb.edu>
2024-02-27 14:39:19 -0800

    update

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ EFFICIENT=lora+prompt-tuning
 STEP_TYPE=vae
 CUDA_VISIBLE_DEVICES=2 python eval.py \
     --base_model_name_or_path $MODEL \
-    --model_name_or_path /mnt/data2/xinyi/reasoning-tuning/main_experiment/train-llama2-7b-math-lora-mlp-prompt-tuning-1-1-vae-5-llama/checkpoint-3737/ \
+    --model_name_or_path your_checkpoint \
     --add_soft_prompts $ADD_SOFT_PROMPT\
     --num_general_prefix_tokens $N_PREFIX \
     --num_special_prefix_tokens $N_SPECIAL \
